f633463d5df5e3538ed6e39d2872a56fa2b8b355
Zia Malik <ziamalik@gmail.com>
2019-07-25 21:20:43 -0400

    Updated to connect to truffle develop network

--- app/src/index.js
+++ app/src/index.js
@@ -66,9 +66,9 @@ window.addEventListener("load", async function() {
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts from Metamask
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:7545. You should remove this fallback when you deploy live",);
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"),);
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
   }
 
   App.start();
